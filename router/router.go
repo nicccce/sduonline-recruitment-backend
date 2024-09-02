664c7f47564a067128840fa88bc238d3064360a0
nicccce <lin2832856512@163.com>
2024-09-03 00:39:36 +0800

    UPDATE
    release版本

--- router/router.go
+++ router/router.go
@@ -3,29 +3,27 @@ package router
 import (
 	"github.com/gin-gonic/gin"
 	"sduonline-recruitment/middleware"
-	"sduonline-recruitment/model"
-	"sduonline-recruitment/pkg/app"
 	"sduonline-recruitment/service"
 )
 
 func Setup(engine *gin.Engine) {
 	// 测试 上线后注释掉
-	test := engine.Group("/test")
-	{
-		//测试panic
-		test.GET("/panic", func(c *gin.Context) {
-			panic("test panic")
-		})
-		//初始化数据库
-		test.GET("/database_initialization", func(c *gin.Context) {
-			aw := app.NewWrapper(c)
-			err := model.Database_initialization()
-			if err != nil {
-				aw.Error(err.Error())
-			}
-			aw.Success("success!")
-		})
-	}
+	/*	test := engine.Group("/test")
+		{
+			//测试panic
+			test.GET("/panic", func(c *gin.Context) {
+				panic("test panic")
+			})
+			//初始化数据库
+			test.GET("/database_initialization", func(c *gin.Context) {
+				aw := app.NewWrapper(c)
+				err := model.Database_initialization()
+				if err != nil {
+					aw.Error(err.Error())
+				}
+				aw.Success("success!")
+			})
+		}*/
 
 	// 用户
 	user := engine.Group("/user")
